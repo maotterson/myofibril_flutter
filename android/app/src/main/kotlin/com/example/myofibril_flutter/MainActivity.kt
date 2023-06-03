270258dc21dae7884916be3d17c0049e94518887
maotterson <otterson.2@gmail.com>
2023-06-03 02:50:35 -0400

    create methodcallhandler for retrieving strava token in android

--- android/app/src/main/kotlin/com/example/myofibril_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/myofibril_flutter/MainActivity.kt
@@ -3,4 +3,36 @@ package com.example.myofibril_flutter
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    private val CHANNEL = "intent"
+
+      override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+          call, result ->
+            if (call.method == "getStravaToken") {
+
+                // todo add values into a config file
+                const authUri = "https://www.strava.com/oauth/mobile/authorize";
+                const clientId = "";
+                const redirectUri = ""; 
+                const responseType = "code";
+                const approvalPrompt = "auto";
+                const scope = "";
+
+                val intentUri = Uri.parse(authUri)
+                    .buildUpon()
+                    .appendQueryParameter("client_id", clientId)
+                    .appendQueryParameter("redirect_uri", redirectUri)
+                    .appendQueryParameter("response_type", responseType)
+                    .appendQueryParameter("approval_prompt", approvalPrompt)
+                    .appendQueryParameter("scope", scope)
+                    .build()
+
+                val intent = Intent(Intent.ACTION_VIEW, intentUri)
+                startActivity(intent)
+
+          } else{
+             result.notImplemented()
+          }
+        }
+      }
 }
